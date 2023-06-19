92e3cc621ad0b343241d260be36754fac931cfac
Mariam Eneji <enejimariam1997@gmail.com>
2023-06-19 10:33:04 +0100

    clean up

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Form from '@/components/Form'
 import Gallery from '@/components/Gallery'
 import Introduction from '@/components/Introduction'
 import Navbar from '@/components/Navbar'
+import Script from 'next/script'
 
 import Image from 'next/image'
 
@@ -11,6 +12,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <div>
+    <Script src="https://third-party-script.js"></Script>
     <Navbar />
     <Introduction />
     <Gallery />
